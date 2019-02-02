4911382fbd954b1177888861d507c76cbddb7203
dianlujitao <dianlujitao@lineageos.org>
2019-02-02 21:08:03 +0800

    Keyguard: Avoid starting FP authentication right after cancelling
     * SystemUI would start FP authentication right after unlock in non-FP
       ways (e.g., pattern, PIN) after e22f1347e8da99676f68864927acbd148dc430a5,
       obviously this is redundant, and can cause unexpected behavior.
    
    Change-Id: Iaa19c3113408d3119b4aebb2b08e9922739267ed
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1297,6 +1297,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         if (mFingerprintRunningState == FINGERPRINT_STATE_RUNNING && !shouldListenForFingerprint) {
             stopListeningForFingerprint();
         } else if (mFingerprintRunningState != FINGERPRINT_STATE_RUNNING
+                && mFingerprintRunningState != FINGERPRINT_STATE_CANCELLING
                 && shouldListenForFingerprint) {
             startListeningForFingerprint();
         }
